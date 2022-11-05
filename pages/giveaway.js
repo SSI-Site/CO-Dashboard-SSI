ac7b742e171a4b38586a15a15b3a93e94839d452
LucasMS115 <lucasm1sales@outlook.com>
2022-11-05 18:27:52 -0300

    Adicionando chamada do sorteio

--- pages/giveaway.js
+++ pages/giveaway.js
@@ -2,6 +2,8 @@ import { React, useState, useEffect } from 'react';
 import styled from 'styled-components';
 import useAuth from '../hooks/useAuth';
 import { useRouter } from 'next/router';
+import saphira from '../services/saphira';
+import { useForm } from "react-hook-form";
 
 import Meta from '../src/infra/Meta';
 import NavBar from '../src/patterns/base/Nav';
@@ -10,9 +12,16 @@ import Button from '../src/components/Button';
 const Giveaway = () => {
     const router = useRouter();
     const { key } = useAuth();
+
+    const placeholderMessage = "Seu nome aparecerá aqui !!";
+
     const [isKeyPresent, setIsKeyPresent] = useState(false);
     const [isLoading, setIsLoading] = useState(false);
-    const [giveawayResultName, setGiveawayResultName] = useState("Seu nome aparecerá aqui !!");
+    const [lectures, setLectures] = useState([{ id: 2, title: "dois" }, { id: 3, title: "tres" }]);
+    const [giveawayResultName, setGiveawayResultName] = useState(placeholderMessage);
+
+    const { register, setError, formState: { errors }, handleSubmit, reset } = useForm();
+
 
     const checkKey = () => {
         if (key) {
@@ -23,17 +32,42 @@ const Giveaway = () => {
         }
     }
 
-    const getGivawayResult = () => {
+    const onSubmit = data => {
+        getGivawayResult(data.lectureId);
+    };
+
+    const getGivawayResult = (lectureId) => {
         setIsLoading(true);
 
         setTimeout(() => {
-            setGiveawayResultName("Lucas M. Sales")
-            setIsLoading(false);
-        }, 1000);
+            saphira.getGivawayResult(lectureId)
+                .then((res) => {
+                    setGiveawayResultName(res.data.nome)
+                    setIsLoading(false);
+                    reset();
+                })
+                .catch(err => {
+                    console.log(err);
+                    setIsLoading(false);
+                    setError("lectureId", { type: "focus" }, { shouldFocus: true })
+                })
+        }, 2000);
+    }
+
+    const listLectures = () => {
+        saphira.getLectures()
+            .then((res) => {
+                console.log(res)
+                setLectures(lectures.concat(...res.data).sort((a, b) => a.id > b.id ? -1 : 1))
+            })
+            .catch((err) => {
+                console.log(err)
+            })
     }
 
     useEffect(() => {
         checkKey();
+        listLectures();
     }, []);
 
     return (
@@ -53,26 +87,48 @@ const Giveaway = () => {
                 <h1>Sorteio</h1>
 
                 {isKeyPresent &&
-                    <ResultSection>
+                    <>
+                        <ResultSection>
+                            {!isLoading &&
+                                <>
+                                    <h2>{giveawayResultName}</h2>
 
-                        {!isLoading &&
-                            <>
-                                <h2>{giveawayResultName}</h2>
+                                    <FormWrapper>
+                                        <form onSubmit={handleSubmit(onSubmit)}>
+                                            <InputBox>
+                                                <label htmlFor='lectureId'> Id da palestra: </label>
+                                                <input id='lectureId' type='text' className={errors.lectureId && 'error-border'}
+                                                    {...register("lectureId", { required: true, minLength: 1, })} />
+                                                {errors.lectureId && <ErrorMessage> Id inválido. </ErrorMessage>}
+                                            </InputBox>
 
-                                {giveawayResultName === "Seu nome aparecerá aqui !!" ?
-                                    <Button onClick={() => getGivawayResult()}> Sortear </Button>
-                                    :
-                                    <Button onClick={() => setGiveawayResultName("Seu nome aparecerá aqui !!")}> Limpar </Button>
-                                }
-                            </>
-                        }
+                                            {giveawayResultName === placeholderMessage &&
+                                                <Button > Sortear </Button>
+                                            }
+                                        </form>
+                                        {giveawayResultName !== placeholderMessage &&
+                                            <Button type="button" onClick={() => setGiveawayResultName(placeholderMessage)}> Limpar </Button>
+                                        }
+                                    </FormWrapper>
+                                </>
+                            }
 
-                        {isLoading &&
-                            <Loading>
-                                <img src='./loading.svg' alt='SSI 2022 - Loading' />
-                            </Loading>
-                        }
-                    </ResultSection>
+                            {isLoading &&
+                                <Loading>
+                                    <img src='./loading.svg' alt='SSI 2022 - Loading' />
+                                </Loading>
+                            }
+                        </ResultSection>
+
+                        <LecturesList>
+                            <ul>
+                                {lectures.map((lecture, key) =>
+                                    <li key={key}>
+                                        id: {lecture.id} | Título: {lecture.title}
+                                    </li>)}
+                            </ul>
+                        </LecturesList>
+                    </>
                 }
             </GiveawayWrapper>
 
@@ -102,12 +158,90 @@ const GiveawayWrapper = styled.div`
 
     padding: 70px 0;
 `
+
+const FormWrapper = styled.div`
+    .error-border {
+        border: .5px solid white;
+    }
+`
+const ErrorMessage = styled.span`
+    color: white;
+    text-decoration: underline 0.5px;
+`
+
+const InputBox = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    position: relative;
+    width: 100%;
+    max-width: 450px;
+    padding: 1.5rem 20px;
+    input {
+        width: 90%;
+        border-radius: 5px;
+        padding: 8px 15px;
+        color: var(--color-text);
+        font-size: 1.6rem;
+    }
+
+    input {
+        border: unset;
+        background-color: #241D3C;
+        filter: brightness(130%);
+
+        width: 100px;
+        border-radius: 5px;
+        padding: 8px 15px;
+
+        color: var(--color-text);
+        font-size: 1.6rem;
+        text-align: center;
+    }
+
+    input:-webkit-autofill,
+    input:-webkit-autofill:hover,
+    input:-webkit-autofill:focus,
+    input:-webkit-autofill:active {
+        -webkit-box-shadow: 0 0 0 30px #241D3C inset;
+        -webkit-text-fill-color: var(--color-text);
+    }
+
+    input::-webkit-outer-spin-button,
+    input::-webkit-inner-spin-button {
+        -webkit-appearance: none;
+        margin: 0;
+    }
+    /* Firefox */
+    input[type=number] {
+        -moz-appearance: textfield;
+    }
+
+    label {
+        color: var(--color-text);
+        font-size: 1.6rem;
+        margin-bottom: 10px;
+    }
+`
+
 const ResultSection = styled.section`
+    height: 30vh;
     margin: 100px auto;
 
     text-align: center;
 
     h2 {
         margin-bottom: 50px;
+    }
+`
+
+const LecturesList = styled.section`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    ul {
+        color: var(--color-text)
     }
 `
--- services/saphira.js
+++ services/saphira.js
@@ -1,24 +1,15 @@
 import axios from 'axios';
-const base_url = 'https://catfact.ninja/fact';
+const BASE_URL = process.env.NEXT_PUBLIC_SAPHIRA_URL;
 
 const saphira = {
 
-  getCatFact: async () => {
-
-    try {
-      const response = await axios.get(base_url);
-      return response.data;
-    } catch (err) {
-      console.log(err);
-    }
-
+  getGivawayResult: async (lectureId) => {
+    const requestUrl = `${BASE_URL}/talk/${lectureId}/draw`
+    return axios.get(requestUrl);
   },
 
-  watchNowTwitch: async () => { },
-
-  sendToken: async parameters => {
-    const { token, user_id } = parameters;
-
+  getLectures: async () => {
+    return axios.get(`${BASE_URL}/talks`);
   }
 
 }
