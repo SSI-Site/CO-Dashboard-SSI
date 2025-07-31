5b3bd4d1d4712085d0a25e614de03034ee8f52a4
F1NH4WK <eduardo.ar334@gmail.com>
2025-07-31 03:20:17 -0300

    fix: hover effect under items

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -20,7 +20,6 @@ const AccordionContainer = styled.div`
     cursor: pointer;
     width: 100%;
     margin-left: 0.5rem;
-    transition: all 200ms ease-in-out;
 
     p {
         font: 400 1rem/1.5rem 'At Aero';
@@ -32,8 +31,12 @@ const AccordionItems = styled.div`
     flex-direction: column;
     gap: 0.75rem;
     padding: 0.75rem 1rem  0rem .75rem;
-    width: fit-content;
     max-height: 0;
+    transition: all 200ms ease-in-out;
+
+    > * {
+        width: fit-content;
+    }
 
     ${props => props.$isOpen && css`
         max-height: unset;
