fc4bcadec20335bded6291d3d5df7d6954018e0c
Douglas <Douglas1412@usp.br>
2026-08-08 00:27:40 -0300

    fix: output: export não suporta rewrites

--- next.config.js
+++ next.config.js
@@ -1,14 +1,24 @@
-module.exports = {
-    output: "export",
-    images: {
-        unoptimized: true,
-    },
-    compiler: {
-        styledComponents: true,
-    },
-    // proxy reverso
-    skipTrailingSlashRedirect: true, // nao adiciona e nem remove / ao final da url
-    async rewrites() {
+// next.config.js
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
+
+module.exports = (phase) => {
+    
+    // Configuração base válida para todos os ambientes
+    const config = {
+        images: { unoptimized: true },
+        compiler: { styledComponents: true },
+    };
+
+    // Se NÃO for modo de desenvolvimento, sem proxy reverso
+    if (phase !== PHASE_DEVELOPMENT_SERVER) {
+        config.output = "export"; 
+        return config; 
+    }
+
+    // Se FOR modo de desenvolvimento, proxy reverso
+    config.skipTrailingSlashRedirect = true; // nao adiciona e nem remove / ao final da url
+    
+    config.rewrites = async () => {
         return [
             {
                 // mantem rotas com / no final
@@ -21,5 +31,7 @@ module.exports = {
                 destination: `${process.env.NEXT_PUBLIC_SAPHIRA_URL}/:path*`
             }
         ]
-    },
+    }
+
+    return config;
 };
