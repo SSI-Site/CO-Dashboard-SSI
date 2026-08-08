2f32f47e7fb18336faf8a3a4cc62b6e5118f1bc8
Rafael Vinicius Nunes <rafaelviniciusnunes@gmail.com>
2026-08-07 22:59:41 -0300

    feat(proxy): adiciona configuração de proxy reverso ao next
    Com a inclusão do servidor de homologação para teste de novas funcionalidades o frontend passa a precisar de um proxy reverse para garantir o funcionameto correto dos cookies estando em cross-site.

--- next.config.js
+++ next.config.js
@@ -4,6 +4,22 @@ module.exports = {
         unoptimized: true,
     },
     compiler: {
-      styledComponents: true,
+        styledComponents: true,
+    },
+    // proxy reverso
+    skipTrailingSlashRedirect: true, // nao adiciona e nem remove / ao final da url
+    async rewrites() {
+        return [
+            {
+                // mantem rotas com / no final
+                source: '/api/:path*/',
+                destination: `${process.env.NEXT_PUBLIC_SAPHIRA_URL}/:path*/`
+            },
+            {
+                // fallback do caso de rota sem / no final
+                source: '/api/:path*',
+                destination: `${process.env.NEXT_PUBLIC_SAPHIRA_URL}/:path*`
+            }
+        ]
     },
 };
